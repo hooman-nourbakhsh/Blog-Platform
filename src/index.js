58f095234cfe67d17d65bd89ba878dca70a65a94
HooMaN <eng.homan@gmail.com>
2025-04-26 19:30:52 +0330

    feat: :tada: Set up Apollo Client with Hygraph in index.js
    I installed @apollo/client and graphql packages, removed unnecessary files, and configured the Apollo Client in index.js to connect to the Hygraph API. I also wrapped the App component with ApolloProvider to enable GraphQL queries across the application.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+
+const client = new ApolloClient({
+  uri: "https://us-west-2.cdn.hygraph.com/content/cm9x4izrr003h07v2kt5usyqs/master",
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
