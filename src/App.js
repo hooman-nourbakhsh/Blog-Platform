dd241b79c94bfee90a2a3cfbe1c5f3818cefa21d
HooMaN <eng.homan@gmail.com>
2025-04-27 18:15:05 +0330

    feat: :sparkles: Add Footer component and Layout structure
    I created the Footer component in src/components/layout/Footer.js using Material UI to display a centered project description. I implemented a Layout component in src/components/layout/index.js to manage Header, content, and Footer. I updated App.js to wrap the HomePage component with the Layout component for a consistent structure.
    
    I created the Footer component in src/components/layout/Footer.js using Material UI to display a centered project description. I implemented a Layout component in src/components/layout/index.js to manage Header, content, and Footer. I updated App.js to wrap the HomePage component with the Layout component for a consistent structure.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import Header from "./components/layout/Header";
+import HomePage from "./components/home/HomePage";
+import Layout from "./components/layout";
 
 function App() {
-  return <Header />;
+  return (
+    <Layout>
+      <HomePage />
+    </Layout>
+  );
 }
 
 export default App;
