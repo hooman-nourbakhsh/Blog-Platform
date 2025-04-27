3f4d9ed9e9060071ceb0a4defc22f23073f67a71
HooMaN <eng.homan@gmail.com>
2025-04-27 20:09:39 +0330

    feat: :sparkles: Implement CardEL component for blog posts
    I developed the CardEL component in src/components/shared/CardEL.js using Material UI to display blog post details, including title, author avatar and name, cover photo, and a study button. I added custom styles for box shadow, border radius, and responsive layout to enhance the card's appearance.

---
+++ src/components/shared/CardEL.js
@@ -0,0 +1,31 @@
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Typography } from "@mui/material";
+
+function CardEL({ title, slug, coverPhoto, author }) {
+  return (
+    <Card sx={{ boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: 4 }}>
+      <CardHeader
+        avatar={<Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />}
+        title={
+          <Typography component={"p"} variant="p" color={"text.primary"}>
+            {author.name}
+          </Typography>
+        }
+      />
+      <CardMedia component={"img"} height={160} image={coverPhoto.url} alt={slug} sx={{ objectFit: "contain" }} />
+
+      <CardContent sx={{ minHeight: 60 }} >
+        <Typography component="h3" variant="body1" color="text.primary" fontWeight={600}>
+          {title}
+        </Typography>
+      </CardContent>
+      <Divider variant="middle" sx={{ margin: "10px" }} />
+      <CardActions>
+        <Button variant="outlined" size="small" sx={{ width: "100%", borderRadius: 3 }}>
+          مطالعه مقاله
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default CardEL;
