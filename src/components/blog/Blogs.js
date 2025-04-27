392eeb88c7623e8551e4788b529b76e2d48c29c7
HooMaN <eng.homan@gmail.com>
2025-04-27 19:01:51 +0330

    feat: :sparkles: Implement blog posts display in Blogs component
    I updated the Blogs component in src/components/blog/Blogs.js to render blog posts using a responsive Material UI Grid layout. I mapped over the fetched posts data and displayed each post using the CardEL component, maintaining loading and error state handling.

--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -1,11 +1,23 @@
 import { useQuery } from "@apollo/client";
+import { Grid } from "@mui/material";
 import { GET_BLOGS_INFO } from "../../graphql/queries";
+import CardEL from "../shared/CardEL";
 
 function Blogs() {
   const { loading, data, error } = useQuery(GET_BLOGS_INFO);
+
   if (loading) return <h4>Loading...</h4>;
   if (error) return <h4>Error....</h4>;
-  return <div>Blogs</div>;
+
+  return (
+    <Grid container spacing={2}>
+      {data.posts.map((post) => (
+        <Grid item xs={12} sm={6} md={4} key={post.id}>
+          <CardEL {...post} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
 export default Blogs;
