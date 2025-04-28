96b31cdf02fbab8ea505f31feb3605f12b3013f0
HooMaN <eng.homan@gmail.com>
2025-04-28 12:23:31 +0330

    feat: :sparkles: I added GraphQL query for post details
    I added a new GraphQL query, GET_POST_INFO, in src/graphql/queries.js to fetch detailed post information, including author details, content, title, and cover photo, based on the post's slug.

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -33,7 +33,7 @@ const GET_AUTHORS_INFO = gql`
 `;
 
 const GET_AUTHOR_INFO = gql`
-  query getAuthorInfo($slug: String) {
+  query getAuthorInfo($slug: String!) {
     author(where: { slug: $slug }) {
       name
       field
@@ -55,4 +55,25 @@ const GET_AUTHOR_INFO = gql`
   }
 `;
 
-export { GET_BLOGS_INFO, GET_AUTHORS_INFO, GET_AUTHOR_INFO };
+const GET_POST_INFO = gql`
+  query getPost($slug: String!) {
+    posts(where: { slug: $slug }) {
+      author {
+        avatar {
+          url
+        }
+        name
+        field
+      }
+      content {
+        html
+      }
+      title
+      coverPhoto {
+        url
+      }
+    }
+  }
+`;
+
+export { GET_BLOGS_INFO, GET_AUTHORS_INFO, GET_AUTHOR_INFO, GET_POST_INFO };
