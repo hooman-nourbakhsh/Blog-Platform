7c3ff25f95f87eb6e6401a47ee33f84a058e3ba7
HooMaN <eng.homan@gmail.com>
2025-04-28 16:16:30 +0330

    feat: :sparkles: I implemented comment submission with mutation and toast notifications
    I installed react-toastify for notifications and created a SEND_COMMENT GraphQL mutation in src/graphql/mutations.js to submit comments. I enhanced the CommentForm component in src/components/comment/CommentForm.js to handle comment submission using useMutation, validate input fields, display toast notifications for success and warnings, and manage the loading state for the submit button.

--- src/components/comment/CommentForm.js
+++ src/components/comment/CommentForm.js
@@ -1,11 +1,40 @@
 import { useState } from "react";
 import { Button, Grid, TextField, Typography } from "@mui/material";
+import { useMutation } from "@apollo/client";
+import { SEND_COMMENT } from "../../graphql/mutations";
+import { ToastContainer, toast } from "react-toastify";
 
 function CommentForm({ slug }) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [text, setText] = useState("");
+  const [sendComment, { loading, data }] = useMutation(SEND_COMMENT, { variables: { name, email, text, slug } });
 
+  const sendHandler = () => {
+    if (name && email && text) {
+      sendComment();
+    } else {
+      toast.warn("وارد کردن تمام فیلد ها اجباری است", {
+        position: "top-center",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: false,
+      });
+    }
+  };
+
+  if (data) {
+    toast.success("نظر شما با موفقیت ثبت شدو منتظر تایید می باشد", {
+      position: "top-center",
+      autoClose: 2500,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: false,
+    });
+  }
   return (
     <Grid container sx={{ boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: 4, py: 1, mt: 5 }}>
       <Grid item xs={12} m={2}>
@@ -31,8 +60,17 @@ function CommentForm({ slug }) {
         />
       </Grid>
       <Grid item xs={12} m={2}>
-        <Button variant="contained">ارسال</Button>
+        {loading ? (
+          <Button variant="contained" disabled>
+            در حال ارسال...
+          </Button>
+        ) : (
+          <Button variant="contained" onClick={sendHandler}>
+            ارسال
+          </Button>
+        )}
       </Grid>
+      <ToastContainer />
     </Grid>
   );
 }
